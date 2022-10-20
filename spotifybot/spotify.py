a9006149b2a2b81076aae642c84333c362f67eb5
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2022-10-20 18:03:42 +0200

    move to method

--- spotifybot/spotify.py
+++ spotifybot/spotify.py
@@ -2,16 +2,22 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
-class Spotify:
+class SpotifyClient:
     def __init__(self):
-        scope = "playlist-modify-private"
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+        self.scope = "playlist-modify-private"
+        self.sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(scope=self.scope, open_browser=True)
+        )
 
-        pl_id = "0E77gEsMy0AuWi5Oi3RHLX"
-        song_url = ["6zmEDMJ9MA4C4ZoPngpz0a"]
+    def add_song_to_playlist(self, playlist: str, song: str):
+        pl_id = playlist
+        song_url = [song]
 
-        result = sp.playlist_add_items(pl_id, song_url)
+        result = self.sp.playlist_add_items(pl_id, song_url)
         print(result)
 
 
-s = Spotify()
+s = SpotifyClient()
+s.add_song_to_playlist("0E77gEsMy0AuWi5Oi3RHLX", "6zmEDMJ9MA4C4ZoPngpz0a")
+
+# spotipy.SpotifyClientCredentials()
